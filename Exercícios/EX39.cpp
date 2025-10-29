b61617ef97fd250455f7a908c23bd181c8c50c20
Arthur do Nascimento Rodrigues <83719255+arthur-nrodrigues@users.noreply.github.com>
2025-10-29 10:18:03 -0300

    Update EX39.cpp

---
+++ Exercícios/EX39.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int var;
+    int *ptr;
+    var = 10;
+    ptr = &var;
+
+    cout << "Endereço de var " << var << endl;
+    cout << &ptr << endl;
+
+    return 0;
+}
